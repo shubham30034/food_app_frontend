05d8c61bd3b2fbd4e54c28540d29c4460aa45bba
shubham kundu <shubhamkundu758@gmail.com>
2024-04-20 10:32:29 +0530

    addPeristanceToToolkit

--- food_app/src/utils/store.jsx
+++ food_app/src/utils/store.jsx
@@ -6,15 +6,31 @@ import loginSlice from "./Slices/loginSlice";
 import MenuSlice from "./Slices/MenuSlice";
 import RestaurantSlice from "./Slices/RestaurantSlice";
 import themeSwitcher from "./Slices/ThemeSlice"
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
+import {combineReducers} from '@reduxjs/toolkit'
+import { version } from "react";
+
+const persistConfig ={
+   key : "root",
+   version : 1,
+   storage
+}
+
+
+const reducer =  combineReducers({
+     login: loginSlice,
+     menu : MenuSlice,
+     restaurant : RestaurantSlice,
+     theme : themeSwitcher,
+     
+})
+
+const persistedReducer = persistReducer(persistConfig,reducer)
 
 
 const store = configureStore({
-     reducer:{
-          login: loginSlice,
-          menu : MenuSlice,
-          restaurant : RestaurantSlice,
-          theme : themeSwitcher,
-     }
+     reducer: persistedReducer
 });
 
 export default store;
